f53ac7ea20ab00250ed65b370d2102d172358724
Lars Gustäbel <lars@gustaebel.de>
2017-05-28 08:35:47 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from wfm import setup
+
+setup(
+    name="pass-applet",
+    description="A small python/gtk3 applet to choose an entry from ZX2C4 pass",
+
+    author="Lars Gustäbel",
+    author_email="lars@gustaebel.de",
+
+    license="MIT",
+    version="0.1",
+
+    scripts=["pass-applet"]
+)
+
